f881b3b7a501ac629c50bee835386f9b9935df6d
Geoff <git@geoffdick.com>
2021-03-10 21:20:27 -0700

    Revert "⚡ feat: Add vue-axios"
    This reverts commit 1a00303e87713e58229eba09ea24580b41f82797.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
-import axios from 'axios'
 import { createApp } from 'vue'
-import VueAxios from 'vue-axios'
 import App from './App.vue'
 import './index.css'
 
-const app = createApp(App)
-app.use(VueAxios, axios)
-app.mount('#app')
-
+createApp(App).mount('#app')
