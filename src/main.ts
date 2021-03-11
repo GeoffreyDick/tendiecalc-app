1a00303e87713e58229eba09ea24580b41f82797
Geoff <git@geoffdick.com>
2021-03-10 21:17:11 -0700

    ⚡ feat: Add vue-axios

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import axios from 'axios'
 import { createApp } from 'vue'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAxios, axios)
+app.mount('#app')
+
