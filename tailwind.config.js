bb9e12384641567e676a9d3b0d560b1a7c365c72
Geoff <git@geoffdick.com>
2021-03-11 15:00:44 -0700

    💄 style: Add basic TailwindCSS styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    fontFamily: {
+      monospace: ["Inconsolata", "monospace"],
+    },
+    extend: {
+      colors: {
+        dark: "#161615",
+        darkish: "#2a2a29",
+        primary: "#daa144",
+        gain: "#ed6a53",
+        loss: "#26ae56",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      ringWidth: ["focus-within"],
+      ringColor: ["focus-within"],
+      ringOpacity: ["focus-within"],
+    },
   },
   plugins: [],
 };
