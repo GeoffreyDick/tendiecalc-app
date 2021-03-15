47a2ff739bc52d6bdc42c1b3a93bb7ebbae7f987
Geoff <git@geoffdick.com>
2021-03-14 22:42:47 -0600

    🏷️ type: Fix implicit any

--- api/exchange-rates.ts
+++ api/exchange-rates.ts
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 export default async function (req: VercelRequest, res: VercelResponse): Promise<void> {
   try {
-    const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD').then(({data}) => data)
+    const response = await axios.get('https://api.exchangeratesapi.io/latest?base=USD').then(({data}: {data: any}) => data)
     res.send(response)
   } catch (error) {
     res.send(error)
--- api/quote.ts
+++ api/quote.ts
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 export default async function (req: VercelRequest, res: VercelResponse): Promise<void> {
   try {
-    const response = await axios.get(`https://finnhub.io/api/v1/quote?symbol=GME&token=${process.env.FINNHUB_API_KEY}`).then(({data}) => data)
+    const response = await axios.get(`https://finnhub.io/api/v1/quote?symbol=GME&token=${process.env.FINNHUB_API_KEY}`).then(({data}: {data: any}) => data)
     res.send(response)
   } catch (error) {
     res.send(error)
